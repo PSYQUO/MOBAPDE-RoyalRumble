9d026e50074757a1b8be40508f7583ee970a77cf
fweezee <jordangana23@gmail.com>
2017-11-12 10:45:52 +0800

    Made starting menu with pulsating effect

--- app/src/main/java/com/example/jords/mobapde_royalrumble/MainActivity.java
+++ app/src/main/java/com/example/jords/mobapde_royalrumble/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.jords.mobapde_royalrumble;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView iv = (TextView) findViewById(R.id.touch_start);
+        ObjectAnimator scaleDown = ObjectAnimator.ofPropertyValuesHolder(
+                iv,
+                PropertyValuesHolder.ofFloat("scaleX", 1.2f),
+                PropertyValuesHolder.ofFloat("scaleY", 1.2f));
+        scaleDown.setDuration(310);
+        scaleDown.setRepeatCount(ObjectAnimator.INFINITE);
+        scaleDown.setRepeatMode(ObjectAnimator.REVERSE);
+        scaleDown.start();
+
+        /*
+        PixelGridView pixelGrid = new PixelGridView(this);
+        pixelGrid.setNumColumns(10);
+        pixelGrid.setNumRows(10);
+
+        setContentView(pixelGrid);
+        */
     }
 }
