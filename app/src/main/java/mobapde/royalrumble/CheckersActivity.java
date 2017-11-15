fa66c6219a2fa405dca8bd10c9990936a47bcbfd
fweezee <jordangana23@gmail.com>
2017-11-15 15:19:31 +0800

    tapos na boiz (i think)

--- app/src/main/java/mobapde/royalrumble/CheckersActivity.java
+++ app/src/main/java/mobapde/royalrumble/CheckersActivity.java
@@ -1,7 +1,15 @@
 package mobapde.royalrumble;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import mobapde.royalrumble.game.Board;
@@ -12,6 +20,9 @@ import static java.lang.Math.abs;
 
 public class CheckersActivity extends AppCompatActivity {
     LinearLayout gridView;
+
+    ImageView pause_btn;
+    Button resume_btn, quit_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,46 @@ public class CheckersActivity extends AppCompatActivity {
 
         gridView = (LinearLayout) findViewById(R.id.grid);
         gridView.addView(pixelGrid);
+
+        pause_btn = (ImageView) findViewById(R.id.pause_btn);
+        pause_btn.setClickable(true);
+
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.pause);
+        final Window window = dialog.getWindow();
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        resume_btn = (Button) dialog.findViewById(R.id.resume_btn);
+        quit_btn = (Button) dialog.findViewById(R.id.quit_btn);
+
+        resume_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        quit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent i = new Intent(CheckersActivity.this, GameMenuActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);*/
+                finish();
+            }
+        });
+
+
+        pause_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+
+            }
+        });
     }
 
 
