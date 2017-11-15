10a745c034ecb84d3e56dadcdf1dd268302b7c81
HsiaoSt <hsiao.stephen5@gmail.com>
2017-11-15 18:27:50 +0800

    Added new files
    Options, Credits, rps Layouts pushing

---
+++ app/src/main/java/mobapde/royalrumble/CreditsActivity.java
@@ -0,0 +1,13 @@
+package mobapde.royalrumble;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreditsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.credits);
+    }
+}
