9799f60b62fac4bde867d51e45f9430c6e7ea38b
fweezee <jordangana23@gmail.com>
2017-12-16 02:10:59 +0800

    tapos na boiz (i think)

--- app/src/main/java/mobapde/royalrumble/OptionsActivity.java
+++ app/src/main/java/mobapde/royalrumble/OptionsActivity.java
@@ -1,5 +1,9 @@
 package mobapde.royalrumble;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +18,7 @@ public class OptionsActivity extends AppCompatActivity
     EditText et_username;
     Button btn_picture;
     SeekBar fx_volume, bg_volume;
+    ImageView player1_pic, player2_pic;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,15 @@ public class OptionsActivity extends AppCompatActivity
         btn_picture = (Button) findViewById(R.id.btn_picture);
         fx_volume = (SeekBar) findViewById(R.id.fx_volume);
         bg_volume = (SeekBar) findViewById(R.id.bg_volume);
+        player1_pic = (ImageView) findViewById(R.id.player1_pic);
+        player2_pic = (ImageView) findViewById(R.id.player2_pic);
+
+        btn_picture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // dispatchTakePictureIntent();
+            }
+        });
 
         back_btn_options.setOnClickListener(new View.OnClickListener()
         {
@@ -34,5 +48,22 @@ public class OptionsActivity extends AppCompatActivity
                 finish();
             }
         });
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            player1_pic.setImageBitmap(imageBitmap);
+        }
     }
 }
