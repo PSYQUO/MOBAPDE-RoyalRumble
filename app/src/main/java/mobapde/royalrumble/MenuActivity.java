305526008caf04f09aa2346701a525ee73fd39bd
Patrick Sy <patrick.alq.sy@gmail.com>
2017-12-16 01:47:31 +0800

    remove System.exit(0)

--- app/src/main/java/mobapde/royalrumble/MenuActivity.java
+++ app/src/main/java/mobapde/royalrumble/MenuActivity.java
@@ -57,7 +57,6 @@ public class MenuActivity extends AppCompatActivity
             public void onClick(View v)
             {
                 finish();
-                System.exit(0);
             }
         });
     }
